4d9d7db8830e7b98009f08c8f043f2f26aa314a0
Caliaha <34219663+Caliaha@users.noreply.github.com>
2017-12-13 13:26:21 -0500

    Added settings.py

---
+++ settings.py
@@ -0,0 +1,54 @@
+import discord
+from discord.ext import commands
+import pymysql.cursors
+from stuff import getSnowflake, superuser, isSuperUser
+
+class Settings():
+	def __init__(self, bot):
+		self.bot = bot
+		self.settings = [ 'phonetic', 'realm', 'character', 'battletag' ]
+
+	@commands.command(pass_context=True)
+	@superuser()
+	async def set(self, ctx, setting, value, member = ''):
+		await self.bot.send_typing(ctx.message.channel)
+		if setting not in self.settings:
+			await self.bot.send_message(ctx.message.channel, 'Usage: !set variable value')
+			return False
+			
+		if value == '':
+			value = None
+		userid = ctx.message.author.id
+		if member:
+			if isSuperUser(ctx):
+				userid = getSnowflake(member)
+				if not userid:
+					return False
+			else:
+				await self.bot.send_message(ctx.message.channel, 'You do not have permission to use this command in this manner')
+				return False
+		else:
+			member = ctx.message.author.name
+			
+		connection = pymysql.connect(host='localhost', user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+
+		try:
+			with connection.cursor() as cursor:
+				#Check if entry exists then update or create one
+				sql = "SELECT `" + setting + "` FROM `usersettings` WHERE `discordID`=%s"
+				cursor.execute(sql, (userid))
+				result = cursor.fetchone()
+				if result is None:
+					sql = "INSERT INTO `usersettings` (`discordID`, `" + setting + "`) VALUES(%s, %s)"
+					cursor.execute(sql, (userid, value))
+					connection.commit()
+				else:
+					sql = "UPDATE `usersettings` SET `" + setting + "` = %s WHERE `discordID` = %s LIMIT 1"
+					cursor.execute(sql, (value, userid))
+					connection.commit()
+				await self.bot.send_message(ctx.message.channel, setting + ' has been set to **' + str(value) + '** for ' + member)
+		finally:
+			connection.close()
+
+def setup(bot):
+	bot.add_cog(Settings(bot))
