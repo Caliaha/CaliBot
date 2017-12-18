789f63f6c4bd1b003d761924931e82275c0edd46
Caliaha <34219663+Caliaha@users.noreply.github.com>
2017-12-18 12:14:38 -0500

    Added reconnection logic

--- announce.py
+++ announce.py
@@ -47,12 +47,16 @@ class Announce():
 			connection.close()
 
 	async def on_voice_state_update(self, before, after):
+		try:
+			if after.name == self.bot.NAME and after.voice.voice_channel is not None: # Bot has moved, update DB for reconnection purposes and return so we don't announce ourselves
+				await self.updateDB(after.server, after.voice.voice_channel)
+				return
+		except:
+			print('Error adding ourselves to db or something weird')
 		server = before.server
 		voiceBefore = before.voice.voice_channel
 		voiceAfter = after.voice.voice_channel
 		if (server not in self.DO_ANNOUNCEMENTS or server not in self.VOICE_CHANNELS):
-			return
-		if after.name == self.bot.NAME:
 			return
 		if voiceBefore is not self.VOICE_CHANNELS[server].channel and voiceAfter is self.VOICE_CHANNELS[server].channel:
 			print(before.name,'has joined the channel')
@@ -70,26 +74,76 @@ class Announce():
 			return
 		return
 
+	async def removeFromDB(self, server, channel):
+		try:
+			connection = pymysql.connect(host='localhost', user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+			with connection.cursor() as cursor:
+				sql = "DELETE FROM `voice_status` WHERE `server`=%s"
+				cursor.execute(sql, (server.id))
+				connection.commit()
+		except:
+			print('Error removing from db or something')
+		finally:
+			connection.close()
+
+	async def updateDB(self, server, channel):
+		await self.removeFromDB(server, channel)
+		try:
+			connection = pymysql.connect(host='localhost', user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+			with connection.cursor() as cursor:
+				sql = "INSERT INTO `voice_status` (`server`, `channel`) VALUES(%s, %s)"
+				cursor.execute(sql, (server.id, channel.id))
+				connection.commit()
+		except:
+			print('Error adding to db or something')
+		finally:
+			connection.close()
+
+	async def joinVoiceChannel(self, server, channel):
+		print(server, channel)
+		self.DO_ANNOUNCEMENTS.append(server)
+		voice = await self.bot.join_voice_channel(channel)
+		self.VOICE_CHANNELS[server] = voice
+		await self.updateDB(server, channel)
+
+	async def leaveVoiceChannel(self, server, channel):
+		await self.VOICE_CHANNELS[server].disconnect()
+		self.DO_ANNOUNCEMENTS.remove(server)
+		del self.VOICE_CHANNELS[server]
+		await self.removeFromDB(server, channel)
+
 	async def joinOrMove(self, ctx):
 		channel = ctx.message.author.voice.voice_channel
 		server = ctx.message.author.server
  
-		if channel is None:
-			await self.bot.send_message(ctx.message.channel, "I couldn't figure out what voice channel you were in.")
-			return False
- 
 		if server in self.VOICE_CHANNELS:
-			if self.VOICE_CHANNELS[server].channel is channel:
-				await self.VOICE_CHANNELS[server].disconnect()
-				self.DO_ANNOUNCEMENTS.remove(ctx.message.server)
-				del self.VOICE_CHANNELS[server]
+			if self.VOICE_CHANNELS[server].channel is channel or channel is None:
+				await self.leaveVoiceChannel(server, channel)
 			else:
-				print('I have moved to another channel on the same guild')
 				await self.VOICE_CHANNELS[server].move_to(channel)
+				await self.updateDB(server, channel)
+		elif channel:
+			await self.joinVoiceChannel(server, channel)
 		else:
-			self.DO_ANNOUNCEMENTS.append(ctx.message.server)
-			voice = await self.bot.join_voice_channel(channel)
-			self.VOICE_CHANNELS[server] = voice
+			await self.bot.send_message(ctx.message.channel, "I couldn't figure out what voice channel you were in.")
+
+	async def on_ready(self):
+		print("Attempting to reconnect to all voice channels")
+		connection = pymysql.connect(host='localhost', user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+		with connection.cursor() as cursor:
+			sql = "SELECT `server`, `channel` FROM `voice_status`"
+			cursor.execute(sql)
+			results = cursor.fetchall()
+			for result in results:
+				print(result['server'], result['channel'])
+				server = self.bot.get_server(result['server'])
+				channel = self.bot.get_channel(result['channel'])
+				if server and channel:
+					await self.joinVoiceChannel(server, channel)
+#		except:
+#			print('Error reconnecting to voice things with db')
+#		finally:
+#			connection.close()
 
 	@commands.command(pass_context=True, description="Bot will join your voice channel and announces who joins or leaves the voice channel you are in, limit one per guild")
 	@no_pm()
@@ -97,16 +151,6 @@ class Announce():
 		"""Bot announces who joins or leaves your voice channel"""
 		await self.joinOrMove(ctx)
 	
-	@commands.command(pass_context=True, description="Force bot to leave voice channel on guild")
-	@no_pm()
-	async def leave(self, ctx):
-		"""Makes bot leave voice channel"""
-		server = ctx.message.author.server
-		if server in self.VOICE_CHANNELS:
-			await self.VOICE_CHANNELS[server].disconnect()
-			self.DO_ANNOUNCEMENTS.remove(ctx.message.server)
-			del self.VOICE_CHANNELS[server]
-
 	@commands.command(pass_context=True, hidden=True)
 	@superuser()
 	@no_pm()
