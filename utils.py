276182fcdd09c3ccfcc4db25a88ae84865caf4f1
Caliaha <34219663+Caliaha@users.noreply.github.com>
2018-02-09 11:22:11 -0500

    Added load and unload cog commands

--- utils.py
+++ utils.py
@@ -42,5 +42,23 @@ class utils():
 		await self.bot.send_message(ctx.message.channel, self.bot.NAME + ' Uptime: ' + '{0.days:01.0f} days {0.hours:01.0f} hours {0.minutes:01.0f} minutes {0.seconds:01.0f} seconds'.format(relativedelta(seconds=time.time() - self.bot.startTime)))
 		await self.bot.send_message(ctx.message.channel, 'Computer Uptime: ' + '{0.days:01.0f} days {0.hours:01.0f} hours {0.minutes:01.0f} minutes {0.seconds:01.0f} seconds'.format(relativedelta(seconds=uptime.uptime())))
 
+	@commands.command(pass_context=True)
+	@isBotOwner()
+	async def load(self, ctx, extension_name : str):
+		"""Loads an extension."""
+		try:
+			self.bot.load_extension(extension_name)
+		except (AttributeError, ImportError) as e:
+			await self.bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+			return
+		await self.bot.say("{} loaded.".format(extension_name))
+
+	@commands.command(pass_context=True)
+	@isBotOwner()
+	async def unload(self, ctx, extension_name : str):
+		"""Unloads an extension."""
+		self.bot.unload_extension(extension_name)
+		await self.bot.say("{} unloaded.".format(extension_name))
+
 def setup(bot):
 	bot.add_cog(utils(bot))
