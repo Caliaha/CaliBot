5d33290dd62e911240aec01b7e7b3cbb47a9914a
Caliaha <34219663+Caliaha@users.noreply.github.com>
2018-10-22 12:31:43 -0400

    Features completed

--- raffle.py
+++ raffle.py
@@ -1,27 +1,41 @@
-import aiohttp
+import aiomysql
 import asyncio
-from bs4 import BeautifulSoup
 import discord
 from discord.ext import commands
-import hashlib
 import pymysql.cursors
 import random
-import re
-from stuff import doThumbs, fetchWebpage
-from urllib.parse import urlparse
-import urllib.request
+import secrets
+from stuff import BoxIt, checkPermissions, doThumbs, sendBigMessage
 
 class Raffle():
 	def __init__(self, bot):
 		self.bot = bot
+		
+	@commands.command()
+	async def rafflehelp(self, ctx):
+		help = { }
+		help['!tickets [@member]'] = 'Lists amount of tickets member has'
+		help['!ticketlist'] = 'Lists tickets of all members in database'
+		help['!drawtest'] = 'Simulates 10,000 draws and show statistics'
+		help['!draw'] = 'Picks winner from ticket holders'
+		help['!giveticket @member [amount]'] = 'Gives one or amount of tickets to member, can be negative'
+		help['!removetickets @member'] = 'Removes all tickets from member'
+		help['!removealltickets'] = 'Removes all tickets from all members of guild'
+
+		helpBox = BoxIt()
+		helpBox.setTitle('Raffle Commands')
+		for h in help:
+			helpBox.addRow([ h, help[h] ])
+		
+		helpBox.setHeader([ 'Command', 'Description' ])
+		await sendBigMessage(self, ctx, helpBox.box())
+		#await ctx.send('```Raffle Commands\n!tickets [@member]			Lists amount of tickets member has\n!ticketlist				Lists tickets of all members in database\n!drawtest				Simulates 10,000 draws and show statistics\nThe following commands are restricted:\n!draw					Picks winner from ticket holders\n!giveticket @member [amount]		Gives one or amount of tickets to member, can be negative\n!removetickets @member			Removes all tickets from member\n!removealltickets			Removes all tickets from all members of guild```')
 
 	@commands.command()
+	@checkPermissions('raffle')
+	@commands.guild_only()
 	@doThumbs()
-	async def giveticket(self, ctx, value: int, member: discord.Member=None):
-		if (member is None):
-			await ctx.send("Usage: !giveticket AMOUNT @member")
-			return False
-
+	async def giveticket(self, ctx, member: discord.Member, value: int = 1):
 		try:
 			connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 			with connection.cursor() as cursor:
@@ -44,6 +58,51 @@ class Raffle():
 			connection.close()
 
 	@commands.command()
+	@checkPermissions('raffle')
+	@commands.guild_only()
+	@doThumbs()
+	async def removetickets(self, ctx, member: discord.Member):
+		try:
+			connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+			with connection.cursor() as cursor:
+				sql = "DELETE FROM `raffle` WHERE `discordID`=%s AND `guildID`=%s LIMIT 1"
+				cursor.execute(sql, (member.id, ctx.guild.id))
+				connection.commit()
+
+				await ctx.send("{} now has no tickets".format(member.nick or member.name))
+				return True
+		finally:
+			connection.close()
+
+	@commands.command()
+	@checkPermissions('raffle')
+	@commands.guild_only()
+	@doThumbs()
+	async def removealltickets(self, ctx):
+		message = await ctx.send('This will remove all tickets from all members of this guild and can not be undone, react with 👌 to confirm.')
+
+		def check(reaction, user):
+			return user == ctx.author and str(reaction.emoji) == '👌'
+
+		try:
+			reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0, check=check)
+		except asyncio.TimeoutError:
+			await message.delete()
+		else:
+			try:
+				connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+				with connection.cursor() as cursor:
+					sql = "DELETE FROM `raffle` WHERE `guildID`=%s"
+					cursor.execute(sql, (ctx.guild.id))
+					connection.commit()
+
+					await ctx.send("All tickets for {} have been deleted".format(ctx.guild.name))
+					await message.delete()
+					return True
+			finally:
+				connection.close()
+
+	@commands.command()
 	@doThumbs()
 	async def tickets(self, ctx, member: discord.Member=None):
 		if (member is None):
@@ -64,33 +123,106 @@ class Raffle():
 		return True
 
 	@commands.command()
+	@checkPermissions('raffle')
+	@commands.guild_only()
 	@doThumbs()
-	async def draw(self, ctx):
+	async def draw(self, ctx, threshold: int = 1):
 		try:
 			connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 			with connection.cursor() as cursor:
 				sql = "SELECT `discordID`, `tickets` FROM `raffle` WHERE `guildID`=%s"
 				cursor.execute(sql, (ctx.guild.id))
 				results = cursor.fetchall()
-				print(cursor.rowcount)
 				count = cursor.rowcount
+
 				weightedList = [ ]
 				for result in results:
 					for i in range(int(result['tickets'])):
-						if int(result['tickets']) > 0:
+						if int(result['tickets']) >= threshold:
 							weightedList.append(result['discordID'])
-				print(weightedList)
+
 				if not weightedList:
 					await ctx.send("There are no members to draw from")
 					return False
 
-				secure_random = random.SystemRandom()
-				winner = secure_random.choice(weightedList)
-				print(winner)
-				
-				member = ctx.guild.get_member(int(winner))
+				secretRandom = secrets.SystemRandom()
+				winner = secretRandom.choice(weightedList)
+				#secure_random = random.SystemRandom()
+				#winner = secure_random.choice(weightedList)
 
-				await ctx.send("{} has won the drawing.".format(member.nick or member.name))
+				try:
+					member = ctx.guild.get_member(int(winner))
+				except:
+					await ctx.send("Failed to get member name form discordID: {}".format(winner))
+
+				await ctx.send("{} has won the drawing.".format(member.mention))
+		finally:
+			connection.close()
+		return True
+
+	@commands.command()
+	@doThumbs()
+	async def drawtest(self, ctx, threshold: int = 1):
+		try:
+			async with ctx.channel.typing():
+				connection = await aiomysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=aiomysql.cursors.DictCursor)
+				async with connection.cursor() as cursor:
+					winners = { }
+					tickets = { }
+					testIterations = 100000
+					for j in range(testIterations):
+						sql = "SELECT `discordID`, `tickets` FROM `raffle` WHERE `guildID`=%s"
+						await cursor.execute(sql, (ctx.guild.id))
+						results = await cursor.fetchall()
+
+						count = cursor.rowcount
+						weightedList = [ ]
+						for result in results:
+							for i in range(int(result['tickets'])):
+								if int(result['tickets']) >= threshold:
+									tickets[result['discordID']] = result['tickets']
+									weightedList.append(result['discordID'])
+
+						secretRandom = secrets.SystemRandom()
+						winner = secretRandom.choice(weightedList)
+						#secure_random = random.SystemRandom()
+						#winner = secure_random.choice(weightedList)
+
+						if winner in winners:
+							winners[winner] = winners[winner] + 1
+						else:
+							winners[winner] = 1
+					
+					box = BoxIt()
+					box.setTitle('Drawing Test with {} iterations'.format(testIterations))
+					for winner in winners:
+						box.addRow([ ctx.guild.get_member(int(winner)), tickets[winner], winners[winner], round((winners[winner] / testIterations) * 100, 2) ])
+					box.sort(2, True)
+					box.setHeader([ 'Winner', 'Tickets', 'Win Counts', 'Win Percentage' ])
+					await sendBigMessage(self, ctx, box.box())
+		finally:
+			connection.close()
+		return True
+
+	@commands.command()
+	@doThumbs()
+	async def ticketlist(self, ctx):
+		try:
+			async with ctx.channel.typing():
+				connection = await aiomysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=aiomysql.cursors.DictCursor)
+				async with connection.cursor() as cursor:
+					sql = "SELECT `discordID`, `tickets` FROM `raffle` WHERE `guildID`=%s"
+					await cursor.execute(sql, (ctx.guild.id))
+					results = await cursor.fetchall()
+
+					box = BoxIt()
+					box.setTitle('Ticket Holders')
+					for result in results:
+						box.addRow([ ctx.guild.get_member(int(result['discordID'])), int(result['tickets']) ])
+
+					box.sort(1, True)
+					box.setHeader([ 'Ticket Holder', 'Tickets' ])
+					await sendBigMessage(self, ctx, box.box())
 		finally:
 			connection.close()
 		return True
