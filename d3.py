d066f53003cd568b966fb7e6f1d46d6fbd86ceef
Caliaha <nagboy@gmail.com>
2019-01-21 00:48:58 -0500

    Additional things

--- d3.py
+++ d3.py
@@ -25,15 +25,14 @@ class Diablo3():
 		self.d3images['demonhunter_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/demonhunter_female.png"
 		self.d3images['monk_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/monk_female.png"
 		self.d3images['monk_male'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/monk_male.png"
-		self.d3images['necro_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/p6_necro_female.png"
-		self.d3images['witchdoctor_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/witchdoctor_female.png"
-		self.d3images['witchdoctor_male'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/witchdoctor_male.png"
+		self.d3images['necromancer_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/p6_necro_female.png"
+		self.d3images['witch-doctor_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/witchdoctor_female.png"
+		self.d3images['witch-doctor_male'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/witchdoctor_male.png"
 		self.d3images['wizard_female'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/wizard_female.png"
 		self.d3images['wizard_male'] = "https://blzmedia-a.akamaihd.net/d3/icons/portraits/100/wizard_male.png"
 		self.d3images['logo'] = "https://blzmedia-a.akamaihd.net/battle.net/logos/og-d3.png"
 
 	async def getAccessToken(self):
-		print(self.bot.WOWAPI_CLIENTID, self.bot.WOWAPI_CLIENTSECRET)
 		auth = aiohttp.BasicAuth(login=self.bot.WOWAPI_CLIENTID, password=self.bot.WOWAPI_CLIENTSECRET)
 		data = { 'grant_type': 'client_credentials' }
 
@@ -46,12 +45,9 @@ class Diablo3():
 						self.accessTokenExpiration = time.time() + int(data['expires_in'])
 					except Exception as e:
 						print('Failed to parse json, getAccessToken', e)
-					
-				print(self.accessToken, self.accessTokenExpiration)
 
 	async def validateAccessToken(self):
 		if self.accessToken == None or self.accessTokenExpiration == None or self.accessTokenExpiration < time.time():
-			print('Token was None or expired, getting new one')
 			await self.getAccessToken()
 			
 	def getBattletag(self, ctx, battletag):
@@ -66,7 +62,6 @@ class Diablo3():
 		
 		if battletagPattern.match(battletag):
 			self.lastLookup[ctx.author.id] = battletag
-			print('Added {} - {} to lastLookup'.format(ctx.author.id, battletag))
 			return battletag
 
 		connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
@@ -81,6 +76,7 @@ class Diablo3():
 				cursor.execute(sql, (discordID))
 				result = cursor.fetchone()
 				if result is not None and result['battletag'] is not None:
+					self.lastLookup[ctx.author.id] = result['battletag']
 					return result['battletag']
 				else:
 					return None
@@ -89,7 +85,7 @@ class Diablo3():
 
 	async def getHeroID(self, hero, battletag):
 		await self.validateAccessToken()
-		
+
 		try:
 			j = json.loads(await fetchWebpage(self, f'https://us.api.blizzard.com/d3/profile/{battletag.replace("#", "%23")}/?locale=en_US&access_token={self.accessToken}'))
 		except Exception as e:
@@ -108,8 +104,7 @@ class Diablo3():
 		await ctx.trigger_typing()
 		await self.validateAccessToken()
 		battletag = self.getBattletag(ctx, battletag)
-		print(battletag)
-		
+	
 		try:
 			j = json.loads(await fetchWebpage(self, f'https://us.api.blizzard.com/d3/profile/{battletag.replace("#", "%23")}/?locale=en_US&access_token={self.accessToken}'))
 		except Exception as e:
@@ -123,12 +118,15 @@ class Diablo3():
 		embed.set_thumbnail(url=self.d3images['logo'])
 		embed.add_field(name='Paragon Levels', value=f'Normal: {j["paragonLevel"]}, Seasonal: {j["paragonLevelSeason"]}, Hardcore: {j["paragonLevelHardcore"]}, Seasonal Hardcore: {j["paragonLevelSeasonHardcore"]}', inline=False)
 		embed.add_field(name='Kills', value=f'Monsters: {j["kills"]["monsters"]}, Elites: {j["kills"]["elites"]}, Hardcore Monsters: {j["kills"]["hardcoreMonsters"]}', inline=False)
+		heroes = []
 		for hero in j['heroes']:
-			embed.add_field(name=f'{hero["name"]}', value=f'Class: {hero["class"]}, Hardcore: {hero["hardcore"]}, Seasonal: {hero["seasonal"]}, Dead: {hero["dead"]}')
+			heroes.append(f'{("", "🍃")[hero["seasonal"]]}{hero["name"]} ({hero["level"]}) {("Male", "Female")[hero["gender"]]} {hero["class"].capitalize()}{("", "Hardcore")[hero["hardcore"]]}{("", "Dead")[hero["dead"]]}')
+			#embed.add_field(name=f'{hero["name"]}', value=f'Class: {hero["class"]}, Hardcore: {hero["hardcore"]}, Seasonal: {hero["seasonal"]}, Dead: {hero["dead"]}')
+		embed.add_field(name='Heroes', value="\n".join(heroes))
 		try:
 			await ctx.send(embed=embed)
 		except discord.HTTPException as e:
-			print(e)
+			print('Beep', e)
 			return False
 		return True
 
@@ -138,19 +136,18 @@ class Diablo3():
 		await ctx.trigger_typing()
 		await self.validateAccessToken()
 		battletag = self.getBattletag(ctx, battletag)
-
 		heroID = await self.getHeroID(hero, battletag)
-		print(battletag, heroID)
+
 		try:
 			j = json.loads(await fetchWebpage(self, f'https://us.api.blizzard.com/d3/profile/{battletag.replace("#", "%23")}/hero/{heroID}?locale=en_US&access_token={self.accessToken}'))
 		except Exception as e:
 			await ctx.send(f'Failed: {e}')
 			return False
-		print(self.d3images[f'{j["class"]}_{("male", "female")[j["gender"]]}'])
+
 		embed=discord.Embed(
 			title=f'{j["name"]} - {("Male", "Female")[j["gender"]]} {j["class"].capitalize()} {battletag}',
 			description = f'Highest Solo Rift: {j["highestSoloRiftCompleted"]}\nParagon Level: {j["paragonLevel"]}\nSeasonal: {j["seasonal"]}, Hardcore: {j["hardcore"]}',
-			url=f'https://us.diablo3.com/en/profile/{battletag.replace("#", "-")}/',
+			url=f'https://us.diablo3.com/en/profile/{battletag.replace("#", "-")}/hero/{heroID}',
 			color=discord.Color(int(self.bot.DEFAULT_EMBED_COLOR, 16)))
 		embed.set_thumbnail(url=self.d3images[f'{j["class"]}_{("male", "female")[j["gender"]]}'])
 
@@ -175,13 +172,46 @@ class Diablo3():
 		for lp in j['legendaryPowers']:
 			kanaisCube.append(lp["name"])
 		#	embed.add_field(name=f'{activeSkill["skill"]["name"]} - {activeSkill["rune"]["name"]}', value=f'{activeSkill["skill"]["description"]}')
-		embed.add_field(name='Active Skills', value="\n".join(activeSkills))
-		embed.add_field(name='Passive Skills', value=", ".join(passiveSkills))
-		embed.add_field(name="Kanai's Cube", value=", ".join(kanaisCube))
+		if activeSkills:
+			embed.add_field(name='Active Skills', value="\n".join(activeSkills))
+		if passiveSkills:
+			embed.add_field(name='Passive Skills', value=", ".join(passiveSkills))
+		if kanaisCube:
+			embed.add_field(name="Kanai's Cube", value=", ".join(kanaisCube))
 		try:
 			await ctx.send(embed=embed)
 		except discord.HTTPException as e:
 			print(e)
+			return False
+		return True
+
+	@commands.command()
+	@doThumbs()
+	async def d3items(self, ctx, hero, battletag = '*'):
+		await ctx.trigger_typing()
+		await self.validateAccessToken()
+		battletag = self.getBattletag(ctx, battletag)
+		heroID = await self.getHeroID(hero, battletag)
+
+		try:
+			j = json.loads(await fetchWebpage(self, f'https://us.api.blizzard.com/d3/profile/{battletag.replace("#", "%23")}/hero/{heroID}?locale=en_US&access_token={self.accessToken}'))
+		except Exception as e:
+			await ctx.send(f'Failed: {e}')
+			return False
+		gear = []
+		for item in j['items']:
+			gear.append(f'{item}: [{j["items"][item]["name"]}](https://us.diablo3.com/en{j["items"][item]["tooltipParams"]})')
+
+		embed=discord.Embed(
+			title=f'{j["name"]} - {("Male", "Female")[j["gender"]]} {j["class"].capitalize()} {battletag}',
+			description = "\n".join(gear),
+			url=f'https://us.diablo3.com/en/profile/{battletag.replace("#", "-")}/hero/{heroID}',
+			color=discord.Color(int(self.bot.DEFAULT_EMBED_COLOR, 16)))
+		embed.set_thumbnail(url=self.d3images[f'{j["class"]}_{("male", "female")[j["gender"]]}'])
+
+		try:
+			await ctx.send(embed=embed)
+		except discord.HTTPException as e:
 			return False
 		return True
 
