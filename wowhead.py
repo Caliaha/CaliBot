8fbb7cf83d41a6dc7ee339600c4bb878799380fb
Caliaha <nagboy@gmail.com>
2019-04-11 19:29:29 -0400

    Added cog stuff

--- wowhead.py
+++ wowhead.py
@@ -9,7 +9,7 @@ from stuff import doThumbs, fetchWebpage
 from urllib.parse import urlparse
 import urllib.request
 
-class WowHead():
+class WowHead(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.bot.loop.create_task(self.background_lookup())
