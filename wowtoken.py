f9b8daefff7c0bf30eccce79f70953d42188f12d
Caliaha <nagboy@gmail.com>
2019-05-26 07:50:29 -0400

    Added wait before loop start

--- wowtoken.py
+++ wowtoken.py
@@ -34,5 +34,9 @@ class WoWToken(commands.Cog):
 			else:
 				await self.bot.change_presence(activity=discord.Game(name=None))
 
+	@tokenUpdateLoop.before_loop
+	async def before_tokenUpdateLoop(self):
+		await self.bot.wait_until_ready()
+
 def setup(bot):
 	bot.add_cog(WoWToken(bot))
