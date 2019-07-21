1d3ebc83edac44339494986a34f2cc34a4d659ed
Caliaha <nagboy@gmail.com>
2019-07-20 22:21:27 -0400

    Now uses tasks instead of sleep loop

--- wowhead.py
+++ wowhead.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
 import pymysql.cursors
 import re
 from stuff import doThumbs, fetchWebpage
@@ -12,89 +12,90 @@ import urllib.request
 class WowHead(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		self.bot.loop.create_task(self.background_lookup())
+		self.updateLoop.start()
 
-	async def background_lookup(self):
-		await self.bot.wait_until_ready()
-		while not self.bot.is_closed():
-			try:
-				newsPage = await fetchWebpage(self, 'http://www.wowhead.com/news')
-			except:
-				print('Unable to fetch wowhead.com/news')
-				
-			#newsPattern = re.compile()
+	def cog_unload(self):
+		self.updateLoop.cancel()
+
+	@tasks.loop(minutes=5.0)
+	async def updateLoop(self):
+		try:
+			newsPage = await fetchWebpage(self, 'http://www.wowhead.com/news')
+		except:
+			print('Unable to fetch wowhead.com/news')
 			
-			soup = BeautifulSoup(newsPage, "html.parser")
+		#newsPattern = re.compile()
+		
+		soup = BeautifulSoup(newsPage, "html.parser")
 
-			for div in soup.find_all('div', class_ = 'news-post news-post-style-teaser'):
-				embed = discord.Embed(title='Test', description='Beep', url='https://www.google.com', color=discord.Color(int(self.bot.DEFAULT_EMBED_COLOR, 16)))
-				postID = div.get('id')
-				if postID is None:
-				 #print('PostID was None, skipping')
-				 continue
+		for div in soup.find_all('div', class_ = 'news-post news-post-style-teaser'):
+			embed = discord.Embed(title='Test', description='Beep', url='https://www.google.com', color=discord.Color(int(self.bot.DEFAULT_EMBED_COLOR, 16)))
+			postID = div.get('id')
+			if postID is None:
+			 #print('PostID was None, skipping')
+			 continue
 
-				for h1 in div.find_all('h1', class_ = 'heading-size-1'):
-					embed.title = h1.text
+			for h1 in div.find_all('h1', class_ = 'heading-size-1'):
+				embed.title = h1.text
 
-				span = div.find('span', class_ = 'date-tip')
+			span = div.find('span', class_ = 'date-tip')
 
-				postTime = span.get('title')
-				author = span.parent.find('a').text
-				if postTime and author:
-					embed.set_footer(text = 'Posted {} by {}'.format(postTime, author))
-				
-				for a in div.find_all('a'):
-					if 'class' in a.attrs:
-						if 'news-post-teaser-image' in a.get('class'):
-							url = a.get('href')
-							#print('Setting url to', url)
-							if not urlparse(url).netloc:
-								url = 'https://www.wowhead.com' + url
-							embed.url = url
-							if 'style' in a.attrs:
-								style = a.get('style')
-								linkPattern = re.compile('url\((.*?\.(jpg|png))')
-								linkMatch = linkPattern.search(style)
-								
-								if (linkMatch):
-									#print(linkMatch[1])
-									print('Added image', linkMatch[1])
-									embed.set_image(url=linkMatch[1]) #url='http://' +
-						if 'news-post-type' in a.get('class'):
-							if (a.text):
-								embed.set_author(name=a.text)
+			postTime = span.get('title')
+			author = span.parent.find('a').text
+			if postTime and author:
+				embed.set_footer(text = 'Posted {} by {}'.format(postTime, author))
+			
+			for a in div.find_all('a'):
+				if 'class' in a.attrs:
+					if 'news-post-teaser-image' in a.get('class'):
+						url = a.get('href')
+						#print('Setting url to', url)
+						if not urlparse(url).netloc:
+							url = 'https://www.wowhead.com' + url
+						embed.url = url
+						if 'style' in a.attrs:
+							style = a.get('style')
+							linkPattern = re.compile('url\((.*?\.(jpg|png))')
+							linkMatch = linkPattern.search(style)
+							
+							if (linkMatch):
+								#print(linkMatch[1])
+								print('Added image', linkMatch[1])
+								embed.set_image(url=linkMatch[1]) #url='http://' +
+					if 'news-post-type' in a.get('class'):
+						if (a.text):
+							embed.set_author(name=a.text)
 
-							url = a.get('href')
-							if not urlparse(url).netloc:
-								url = 'https://www.wowhead.com' + url
-							embed.url = url
+						url = a.get('href')
+						if not urlparse(url).netloc:
+							url = 'https://www.wowhead.com' + url
+						embed.url = url
 
-				descriptionPattern = re.compile('WH\.markup\.printHtml\(\"(.*?)\", \"news')
-				descriptionPattern2 = re.compile('<noscript>(.*?)<\/noscript>')
-				noscript = str(div.find('noscript'))
-				
-				if noscript:
-					descriptionMatch = descriptionPattern2.search(noscript)
-					if descriptionMatch:
-						description = self.subMarkup(descriptionMatch[1], 'html')
-						embed.description = description
-						#print('html', description)
-				else:
-					descriptionMatch = descriptionPattern.search(div.text)
-					if descriptionMatch:
-						description = self.subMarkup(descriptionMatch[1], 'bb')
-						embed.description = description
-						#print('bb', description)
+			descriptionPattern = re.compile('WH\.markup\.printHtml\(\"(.*?)\", \"news')
+			descriptionPattern2 = re.compile('<noscript>(.*?)<\/noscript>')
+			noscript = str(div.find('noscript'))
+			
+			if noscript:
+				descriptionMatch = descriptionPattern2.search(noscript)
+				if descriptionMatch:
+					description = self.subMarkup(descriptionMatch[1], 'html')
+					embed.description = description
+					#print('html', description)
+			else:
+				descriptionMatch = descriptionPattern.search(div.text)
+				if descriptionMatch:
+					description = self.subMarkup(descriptionMatch[1], 'bb')
+					embed.description = description
+					#print('bb', description)
 
-				for guild in self.bot.guilds:
-					for channel in guild.text_channels:
-						if channel.name == 'wowhead':
-							if not await self.checkIfPosted(guild.id, postID):
-								#print("Not posted")
-								await channel.send(embed=embed)
-								await self.storePostedData(guild.id, postID)
-							break
-			await asyncio.sleep(600)
+			for guild in self.bot.guilds:
+				for channel in guild.text_channels:
+					if channel.name == 'wowhead':
+						if not await self.checkIfPosted(guild.id, postID):
+							#print("Not posted")
+							await channel.send(embed=embed)
+							await self.storePostedData(guild.id, postID)
+						break
 
 	def subMarkup(self, text, type):
 		def urlFix(match):
@@ -222,6 +223,10 @@ class WowHead(commands.Cog):
 		finally:
 			connection.close()
 		return True
+
+	@updateLoop.before_loop
+	async def before_updateLoop(self):
+		await self.bot.wait_until_ready()
 
 def setup(bot):
 	bot.add_cog(WowHead(bot))
