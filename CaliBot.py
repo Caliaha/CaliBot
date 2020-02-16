16cbe587f9366723feac06bca03374076c0902c5
Caliaha <nagboy@gmail.com>
2020-02-16 02:33:27 -0500

    Load cogs from subdirectory and not from list

--- CaliBot.py
+++ CaliBot.py
@@ -2,9 +2,10 @@
 import config
 import discord
 from discord.ext import commands
+import os
 import sys
 import time
-#418.91 Feb 13, 2019
+
 # Authorize bot link: https://discordapp.com/oauth2/authorize?&client_id=362335676248621068&scope=bot&permissions=271932480
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='CaliBot')
@@ -23,18 +24,24 @@ bot.TTS_PROGRAM = config.TTS_PROGRAM
 bot.TTS_FILE = config.TTS_FILE
 bot.DEFAULT_EMBED_COLOR = '444580'
 bot.USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'
+bot.COG_DIRECTORY = config.COG_DIRECTORY
+bot.COG_DIRECTORY_DISABLED = config.COG_DIRECTORY_DISABLED
 
 bot.SESSION = aiohttp.ClientSession(loop=bot.loop)
-
-startup_extensions = [ 'announce', 'color', 'permissions', 'raffle', 'settings', 'utils', 'wow', 'playing', 'wowhead', 'monsterhunter' ]
 
 @bot.event
 async def on_ready():
 	print('Logged in as:\n{0} (ID: {0.id})'.format(bot.user))
 
 if __name__ == "__main__":
-	for extension in startup_extensions:
-		bot.load_extension(extension)
-		print("Loaded cog: ", extension)
+	for filename in os.listdir(f'./{bot.COG_DIRECTORY}'):
+		if not os.path.isfile(f'./{bot.COG_DIRECTORY}/{filename}'):
+			continue
+		(file, ext) = os.path.splitext(filename)
+		if not ext == '.py':
+			continue
+		bot.load_extension(f'{bot.COG_DIRECTORY}.{file}')
+		print("Loaded cog: ", file)
+
 
 	bot.run(config.DISCORD_TOKEN_TEST)
