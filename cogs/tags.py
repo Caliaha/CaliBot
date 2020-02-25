d6c792a867f48303dbc7156bb3f00643c8341112
Caliaha <nagboy@gmail.com>
2020-02-24 22:48:59 -0500

    Added Box option

--- cogs/tags.py
+++ cogs/tags.py
@@ -1,12 +1,33 @@
 import discord
 from discord.ext import tasks, commands
 import pymysql.cursors
-from stuff import doThumbs
+from stuff import BoxIt, doThumbs, sendBigMessage
 
 class Tag(commands.Cog):
 	"""TBD"""
 	def __init__(self, bot):
 		self.bot = bot
+
+	def formatMessage(self, text):
+		if text.startswith('/box/'):
+			box = BoxIt()
+			for line in text.splitlines():
+				if line.startswith('/box/'):
+					if line == '/box/s':
+						box.seperateEveryLine = True
+					continue
+
+				
+				entries = line.split('|')
+				if entries[0] == 'h':
+					box.setHeader( entries[1:] )
+				elif entries[0] == 't':
+					box.setTitle(entries[1])
+				else:
+					box.addRow( entries )
+			return box.box(), True
+		else:
+			return text, False
 
 	@commands.group(invoke_without_command=True)
 	@doThumbs()
@@ -30,8 +51,32 @@ class Tag(commands.Cog):
 						await ctx.send(f'No tag found for {name}')
 						
 				else:
-					await ctx.send(result['text'])
+					text, big = self.formatMessage(result['text'])
+					
+					if big:
+						await sendBigMessage(self, ctx, text, '```', '```')
+					else:
+						await ctx.send(text)
 					return True
+		except Exception as e:
+			print(e)
+
+	@tag.command()
+	@doThumbs()
+	async def raw(self, ctx, *, name: str):
+		try:
+			connection = pymysql.connect(host=self.bot.MYSQL_HOST, user=self.bot.MYSQL_USER, password=self.bot.MYSQL_PASSWORD, db=self.bot.MYSQL_DB, charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+			with connection.cursor() as cursor:
+				sql = "SELECT `text` FROM `tags` WHERE `name` = %s"
+				cursor.execute(sql, (name))
+				result = cursor.fetchone()
+				if result:
+					text = result['text']
+					await ctx.send(text)
+					return True
+				else:
+					await ctx.send('No tag found')
+					return False
 		except Exception as e:
 			print(e)
 
